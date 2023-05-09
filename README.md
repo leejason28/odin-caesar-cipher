e160df37ff5bda21d2e018426de33ba34514e40f
leejason28 <jason928lee@gmail.com>
2023-05-08 23:08:19 -0700

    set basic skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-caesar-cipher
+
+odin project: implements a caesar cipher in Ruby
---
+++ cipher.rb
@@ -0,0 +1,4 @@
+# takes a string and returns its characters shifted by sf places in the alphabet 
+def caesar_cipher(string, sf)
+
+end
